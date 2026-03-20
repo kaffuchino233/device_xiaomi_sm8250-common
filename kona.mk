0f17e3d076a21ad2635bb75d32891af9f3352d23
HELLBOY017 <abhaygill017@gmail.com>
2026-03-20 17:13:21 +0800

    sm8250-common: Switch to common overlay component
    kaffchino233:
    * based on commit a5a3c60f5c13140d3591a5915b7f3c9067fe88ba
    
    Change-Id: I99dc5204dfd8ad415d8e47dffc7690ff17630319

--- kona.mk
+++ kona.mk
@@ -261,17 +261,12 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 
 # Overlays
 PRODUCT_PACKAGES += \
-    CarrierConfigOverlayCommon \
-    FrameworkResOverlayCommon \
-    LineageSDKOverlayCommon \
-    LineageSettingsOverlayCommon \
-    SettingsOverlayCommon \
-    SettingsProviderOverlayCommon \
-    SystemUIOverlayCommon \
-    TelephonyOverlayCommon \
-    WifiResourcesOverlayCommon
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
+    MiKonaCarrierConfig \
+    MiKonaFrameworksRes \
+    MiKonaSettingsOverlay \
+    MiKonaSettingsProviderOverlay \
+    MiKonaSystemUIOverlay \
+    MiKonaWifiRes
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -290,6 +285,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
     alarm \
     av \
+    overlay \
     perf \
     wlan
 
